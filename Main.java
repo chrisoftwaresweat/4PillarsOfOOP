238942cbda84f74ef9cb4b4ac63730f8c279fa95
Christian A. Gulfan <christiangulfan366@gmail.com>
2026-08-31 10:03:09 +0800

    Implement student input and display functionality
    Added user input for student details and displayed them.

--- Main.java
+++ Main.java
@@ -1,7 +1,19 @@
 public class Main {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
         Rectangle rectangle = new Rectangle();
         Circle circle = new Circle();
+
+        System.out.print("Enter the name of the student: ");
+        String name = scanner.nextLine();
+        System.out.print("Enter the age of the student: ");
+        int age = scanner.nextInt();
+        System.out.print("Enter the student gpa: ");
+        double gpa = scanner.nextDouble();
+        
+        Student student = new Student(name, age, gpa);
+
+        student.display();
 
         //POLYMORPHISM
         Shape[] shape = {rectangle, circle};
@@ -9,5 +21,7 @@ public class Main {
         for (Shape x : shape) {
             x.draw();
         }
+
+        scanner.close();
     }
 }
